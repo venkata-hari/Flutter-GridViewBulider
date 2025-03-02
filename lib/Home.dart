b7c879471b153bce5137433c47f9a8391ac91ee3
venkata-hari <xprogrammer.me@gmail.com>
2025-03-02 12:23:12 +0530

    first commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+
 class Home extends StatelessWidget {
-  const Home({super.key});
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Flutter App',
+          'Logo',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
+        backgroundColor: Colors.blue,
         actions: [
           IconButton(
             onPressed: () {
@@ -16,94 +16,142 @@ class Home extends StatelessWidget {
                 context: context,
                 builder: (context) {
                   return AlertDialog(
-                    title: Text('Notifications'),
-                    content: Text('You have 3 Notifications'),
+                    title: Text('Notification'),
+                    content: Text('You have 5 Notifications'),
                     actions: [
                       TextButton(
                         onPressed: () {
                           Navigator.pop(context);
                         },
-                        child: Text('Close'),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Text(
+                            'Close',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
                       ),
                     ],
                   );
                 },
               );
             },
-            icon: Icon(Icons.notifications, color: Colors.white),
+
+            icon: Icon(Icons.notification_add, color: Colors.white),
           ),
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.shopping_cart, color: Colors.white),
+            icon: Icon(Icons.logout, color: Colors.white),
           ),
         ],
-        toolbarHeight: 60,
-        backgroundColor: Colors.black,
       ),
-      body: HomeBody(),
+      body: Body(),
     );
   }
 }
 
-class HomeBody extends StatelessWidget {
+class Body extends StatelessWidget {
   @override
   var data = [
-    {"id": 1, "todo": "Memorize a poem", "completed": true, "userId": 13},
-    {"id": 2, "todo": "Memorize a poem", "completed": true, "userId": 13},
-    {"id": 3, "todo": "Watch a classic movie", "completed": true, "userId": 68},
-    {"id": 4, "todo": "Watch a documentary", "completed": false, "userId": 84},
-    {"id": 5, "todo": "Memorize a poem", "completed": true, "userId": 13},
-    {"id": 6, "todo": "Memorize a poem", "completed": true, "userId": 13},
-    {"id": 7, "todo": "Watch a classic movie", "completed": true, "userId": 68},
-    {"id": 8, "todo": "Watch a documentary", "completed": false, "userId": 84},
-    {"id": 9, "todo": "Memorize a poem", "completed": true, "userId": 13},
-    {"id": 10, "todo": "Memorize a poem", "completed": true, "userId": 13},
-    {
-      "id": 11,
-      "todo": "Watch a classic movie",
-      "completed": true,
-      "userId": 68,
-    },
-    {"id": 12, "todo": "Watch a documentary", "completed": false, "userId": 84},
+    {'id': 1, 'name': 'India'},
+    {'id': 2, 'name': 'China'},
+    {'id': 3, 'name': 'Pakistan'},
+    {'id': 4, 'name': 'USA'},
+    {'id': 5, 'name': 'Japan'},
+    {'id': 6, 'name': 'Australia'},
+    {'id': 7, 'name': 'Kenya'},
+    {'id': 8, 'name': 'Finland'},
+    {'id': 9, 'name': 'Norway'},
+    {'id': 10, 'name': 'Russia'},
+    {'id': 11, 'name': 'Srilanka'},
+    {'id': 12, 'name': 'Nepal'},
+    {'id': 13, 'name': 'Afganistan'},
+    {'id': 9, 'name': 'Ukrian'},
   ];
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10),
-      child: SizedBox(
-        // Adjust based on available space
-        child: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, index) {
-            var item = data[index];
-            return Container(
-              key: ValueKey(item['id']),
-              width: double.infinity,
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.symmetric(vertical: 5),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    item['todo'] as String,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    item['completed'] as bool ? 'Completed' : 'Not Completed',
-                    style: TextStyle(
-                      color:
-                          item['completed'] as bool ? Colors.green : Colors.red,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: data.length,
+              itemBuilder:
+                  (context, index) => Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    margin: EdgeInsets.all(2),
+                    child: Row(
+                      children: [
+                        Text(
+                          data[index]['name'] as String,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 3,
+                mainAxisSpacing: 3,
+                childAspectRatio: 2,
+                crossAxisCount: 2,
               ),
-            );
-          },
-        ),
+              itemCount: data.length,
+              itemBuilder:
+                  (context, index) => Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(color: ((data[index]['id'] as int) % 2 != 0 )? Colors.blue : Colors.red),
+                    child: Column(
+                   crossAxisAlignment:CrossAxisAlignment.stretch,
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child:Align(
+                            alignment: Alignment.centerRight,
+                          child:Text(
+                            data[index]['name'] as String,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                            ),
+                          ))),
+                          Expanded(child:Align(
+                            alignment: Alignment.centerLeft,
+                          child:Text(data[index]['name'] as String,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                            ),))), 
+                           Expanded(child:Align(
+                          alignment:Alignment.center,
+                          child:Text(data[index]['name'] as String,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                            ),))),
+                      ],
+                    ),
+                  ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/Home.dart';
-
-void main() {
+void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+
   @override
-  
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home());
+      home:Home()
+    );
   }
 }
